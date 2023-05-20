514a37f3bfdb8c82d24585e642554135dc3dba0f
wzy <3455964760@qq.com>
2023-05-20 15:35:18 +0800

    伟大的史诗级创作项目的开端

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -12,16 +12,11 @@ import java.util.Map;
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo/")
-    public List<Map<String,String>> getinfo(){
-            List<Map<String,String >> list = new LinkedList<>();
+    public Map<String,String> getinfo(){
+
             Map<String ,String> bot1 = new HashMap<>();
             bot1.put("name","tager");
             bot1.put("rating","1500");
-        Map<String ,String> bot2 = new HashMap<>();
-        bot2.put("name","apple");
-        bot2.put("rating","1800");
-        list.add(bot1);
-        list.add(bot2);
-        return list;
+        return bot1;
     }
 }
